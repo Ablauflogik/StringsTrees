fe4ffc5f5e4fc272f9ed1676d55599fee7a7b7ad
Montenegro <lauflogik@gmail.com>
2019-12-10 23:59:00 -0600

    Added changes and Tree

---
+++ StringCollections/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCollections
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UsingTree();
+            //UsingList();
+        }
+        public static void UsingList()
+        {
+
+            TimeSpan sortTime;
+            TimeSpan ZTime;
+            TimeSpan HeTime;
+            TimeSpan eTime;
+            DateTime start;
+
+            List<string> words = ReadInWords("words.txt");
+
+            start = DateTime.Now;
+            words.Sort((a, b) => b.CompareTo(a));
+            sortTime = DateTime.Now - start;
+
+            ///PrintWords(words, "Full List");
+
+            start = DateTime.Now;
+            List<string> wordsBeginWithZ = words.Where(x => x.ToLower().StartsWith("z")).ToList();
+            ZTime = DateTime.Now - start;
+
+            /*
+            Console.WriteLine("   {0,-35} {1,20}", "Value of Minutes Component:", interval.Minutes);
+            Console.WriteLine("   {0,-35} {1,20}", "Total Number of Minutes:", interval.TotalMinutes);
+            Console.WriteLine("   {0,-35} {1,20:N0}", "Value of Seconds Component:", interval.Seconds);
+            */
+            PrintWords(wordsBeginWithZ, "Begins with Z");
+
+
+
+            start = DateTime.Now;
+            List<string> wordsBeginWithHe = words.Where(x => x.ToLower().StartsWith("he")).ToList();
+            HeTime = DateTime.Now - start;
+
+            PrintWords(wordsBeginWithHe, "Begins with He");
+
+
+            start = DateTime.Now;
+            List<string> wordsWithEAtSecondPos = words.Where(x => x != null && x.Length > 1 && x.ToLower()[1] == 'e').ToList();
+            eTime = DateTime.Now - start;
+            PrintWords(wordsWithEAtSecondPos, "Has e second position");
+
+
+            Console.WriteLine("Total Time Sort: {0,2:N0}:{1,2:N0}", sortTime.Seconds, sortTime.Milliseconds);
+            Console.WriteLine("Total Time Begins with Z: {0,2:N0}:{1,2:N0}", ZTime.Seconds, ZTime.Milliseconds);
+            Console.WriteLine("Total Time He: {0,2:N0}:{1,2:N0}", HeTime.Seconds, HeTime.Milliseconds);
+            Console.WriteLine("Total Time E: {0,2:N0}:{1,2:N0}", eTime.Seconds, eTime.Milliseconds);
+
+            Console.ReadLine();
+        }
+        public static void UsingTree()
+        {
+
+            TimeSpan sortTime;
+            TimeSpan ZTime;
+            TimeSpan HeTime;
+            TimeSpan eTime;
+            DateTime start;
+
+            List<string> words = ReadInWords("words.txt");
+            Tree<string> tree = new Tree<string>();
+            
+            for (char i = 'a'; i < 'z'; i++)
+            {
+                tree.Root.AddChild(i.ToString());
+            }
+
+            
+            start = DateTime.Now;
+            words.Sort((a, b) => b.CompareTo(a));
+            sortTime = DateTime.Now - start;
+
+            foreach (var word in words)
+            {
+                if (word[0] >= 'a' && word[0] <= 'z')
+                {
+                    tree.Root.Nodes[word[0] - 'a'].AddChild(word);
+                }
+                else
+                {
+                    tree.Root.AddChild(word);
+                }
+            }
+
+            ///PrintWords(words, "Full List");
+
+            start = DateTime.Now;
+            List<string> wordsBeginWithZ = tree.Where(x => x.ToLower().StartsWith("z")).ToList();
+            ZTime = DateTime.Now - start;
+
+            /*
+            Console.WriteLine("   {0,-35} {1,20}", "Value of Minutes Component:", interval.Minutes);
+            Console.WriteLine("   {0,-35} {1,20}", "Total Number of Minutes:", interval.TotalMinutes);
+            Console.WriteLine("   {0,-35} {1,20:N0}", "Value of Seconds Component:", interval.Seconds);
+            */
+            PrintWords(wordsBeginWithZ, "Begins with Z");
+
+
+
+            start = DateTime.Now;
+            List<string> wordsBeginWithHe = tree.Where(x => x.ToLower().StartsWith("he")).ToList();
+            HeTime = DateTime.Now - start;
+
+            PrintWords(wordsBeginWithHe, "Begins with He");
+
+
+            start = DateTime.Now;
+            List<string> wordsWithEAtSecondPos = tree.Where(x => x != null && x.Length > 1 && x.ToLower()[1] == 'e').ToList();
+            eTime = DateTime.Now - start;
+            PrintWords(wordsWithEAtSecondPos, "Has e second position");
+
+
+            Console.WriteLine("Total Time Sort: {0,2:N0}:{1,2:N0}", sortTime.Seconds, sortTime.Milliseconds);
+            Console.WriteLine("Total Time Begins with Z: {0,2:N0}:{1,2:N0}", ZTime.Seconds, ZTime.Milliseconds);
+            Console.WriteLine("Total Time He: {0,2:N0}:{1,2:N0}", HeTime.Seconds, HeTime.Milliseconds);
+            Console.WriteLine("Total Time E: {0,2:N0}:{1,2:N0}", eTime.Seconds, eTime.Milliseconds);
+
+            Console.ReadLine();
+        }
+        public static void PrintWords(List<string> words, string header)
+        {
+
+            Console.WriteLine("*****" + header + "*****");
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        public static List<string> ReadInWords(string fileName)
+        {
+            List<string> wordList = new List<string>();
+            using (StreamReader streamReader = new StreamReader(fileName))
+            {
+
+                while (!streamReader.EndOfStream)
+                {
+                    string word = streamReader.ReadLine();
+                    wordList.Add(word);
+                }
+            } //close off using statement ... closes file stream and disposes stream reader
+            return wordList;
+        }
+    }
+}
---
+++ StringCollections/Tree.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCollections
+{
+    class Tree<T> where T : IComparable
+    {
+
+        TreeNode<T> _root;
+
+        public Tree()
+        {
+            _root = new TreeNode<T>(default(T));
+        }
+
+        public TreeNode<T> Root
+        {
+            get
+            {
+                return _root;
+            }
+        }
+
+        public List<T> GetAll()
+        {
+            List<T> allItems = new List<T>();
+            GetAllHelper(_root, allItems);
+
+            return allItems;
+        }
+
+
+        public void AppendChildTo(T targetParent, T child)
+        {
+            //TODO
+        }
+
+        public T Find(Predicate<T> match)
+        {
+            List<T> list = GetAll();
+
+            T result = list.Find(match);
+
+            return result;
+        }
+        public IEnumerable<T> Where(Func<T,bool> match)
+        {
+            List<T> list = GetAll();
+            if (list == null || list.Count < 1)
+            {
+                return new List<T>();
+            }
+            IEnumerable<T> result = list.Where(match);
+
+            return result;
+        }
+
+
+        private void GetAllHelper(TreeNode<T> root, List<T> list)
+        {
+            if (root != null)
+            {
+                if (root.Data != null)
+                {
+                    list.Add(root.Data);
+                }
+                      
+                foreach (var node in root.Nodes)
+                {
+                    GetAllHelper(node, list);
+                }
+            }
+        }
+    }
+
+
+    class TreeNode<T> : IComparable where T : IComparable
+    {
+
+        public T Data { get; set; }
+
+        //TODO... possible option to use LinkedList for traversing siblings
+        private List<TreeNode<T>> _children;
+        private TreeNode<T> _parent;
+
+        public TreeNode(T data)
+        {
+            Data = data;
+            _children = new List<TreeNode<T>>();
+        }
+
+        public void AddChild(T data)
+        {
+
+            TreeNode<T> node = new TreeNode<T>(data);
+            node._parent = this;
+            _children.Add(node);
+        }
+
+        public bool RemoveChild(T data)
+        {
+            int countBefore = _children.Count;
+            _children.Remove(new TreeNode<T>(data));
+
+            int countAfter = _children.Count;
+
+            return countAfter != countBefore;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            TreeNode<T> other = (TreeNode<T>)obj;
+            T data = other.Data;
+            if (other != null)
+                return this.Data.CompareTo(data);
+            else
+                throw new ArgumentException($"Object is not a {nameof(T)}");
+        }
+
+        public override bool Equals(object o)
+        {
+            TreeNode<T> other = o as TreeNode<T>;
+            return other.Data.Equals(Data);
+        }
+
+
+        public override int GetHashCode()
+        {
+
+            return Data.GetHashCode();
+        }
+
+        public List<TreeNode<T>> Nodes
+        {
+            get
+            {
+                return _children;
+            }
+            private set { }
+        }
+
+        public TreeNode<T> Parent
+        {
+            get
+            {
+                return _parent;
+            }
+            private set { }
+        }
+    }
+}
